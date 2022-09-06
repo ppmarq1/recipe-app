e770a76b9158a7400f8c3ec616b27405e7c21fe4
Pedro Marques <ppmarq1@gmail.com>
2022-09-06 17:26:06 +0100

    Add specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
-  get 'foods/index'
+  get 'recipe_food/index'
+  
   devise_for :users
 
-  resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   #root "users#index"
 
   # Defines the root path route ("/")
 
-   root "users#index"
+  root 'foods#index'
 
+
+  resources :users
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy]
+  end
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
 end
-
--- db/migrate/20220906142127_add_foods_ref_to_recipes_foods.rb
+++
@@ -1,5 +0,0 @@
-class AddFoodsRefToRecipesFoods < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :recipes_foods, :food, null: false, foreign_key: true
-  end
-end
