94f4866f1af1b5893d46fccc1447bc396ca468bb
Pedro Marques <ppmarq1@gmail.com>
2022-09-07 08:36:39 +0100

    Add spec features

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Dudu') }
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'checking if the user is admin' do
+    expect(subject.admin?).to be_falsey
+  end
 end
