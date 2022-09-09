32a088038b52d74eafb898723a07904a65467a94
tugsem <saranlitugsem@gmail.com>
2022-09-09 16:15:32 +0300

    Change recipes order to newest

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true)
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 
 end
